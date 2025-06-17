97258e2d8c33aa8b28a9a25bbb9f6560ee6977fe
Deepak Kumar <1172dipu@gmail.com>
2025-06-17 18:16:22 +0530

    fix alb

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -3,7 +3,7 @@ resource "aws_lb" "app_alb" {
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.alb_sg.id]
-  subnets            = aws_subnet.public[*].id
+  subnets            = var.public_subnets
   enable_deletion_protection = false
 
   tags = {
--- modules/alb/variables.tf
+++ modules/alb/variables.tf
@@ -1,4 +1,7 @@
 # modules/alb/variables.tf
 variable "vpc_id" {}
-variable "public_subnets" { type = list(string) }
+variable "public_subnets" { 
+    type = list(string) 
+    description = "List of public subnet IDs to attach to ALB"
+}
 variable "target_group_port" {}
